c1391b16a6e09c8e1b6b2164aa043972c8f00cef
Balla Ganesh <ganeshballa176@gmail.com>
2025-06-25 14:01:30 +0530

    AddBills completed

--- bulidapp/src/components/user/main/addBills/AddBills.jsx
+++ bulidapp/src/components/user/main/addBills/AddBills.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddBills = () => {
+  const [Addbills,setbills]=useState({
+    companyName:"",
+    PoNo:"",
+    workCompletionDate:"",
+    address:"",
+    PAN:"",
+    GSTNo:"",
+    clientBankName:""
+  })
+  const handleChange=(e)=>{
+    const {name,value}=e.target
+    setbills((preval)=>({...preval,[name]:value}))
+
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(Addbills);
+
+  }
+
   return (
-    <div>AddBills</div>
+    <div className='bg-[#efefef]  size-full flex justify-center items-center gap-10 px-25 '>
+            <form action="" className='bg-red-100 h-[70%] overflow-hidden w-1/2 shadow-2xl rounded-2xl  flex gap-5 px-[80px] flex-col max-sm:w-[90%] ' onSubmit={handleSubmit}>
+          <div className='font-bold h-30 text-3xl  flex justify-center items-center max-lg '>
+            <h1  className='mt-20'>Add Bills</h1>
+           </div>
+    
+          <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 ' >
+            <input type="text" name="companyName" placeholder='Enter Company Name' className='outline-0  px-9 h-7  w-full' onChange={handleChange} />
+            
+          </div>
+    
+           <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="text" name="PoNo" placeholder='Enter PoNo' className='outline-0 px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+    
+           <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="date" name="workCompletionDate" placeholder='Enter workCompletionDate' className='outline-none px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+          <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="text" name="address" placeholder='Enter address' className='outline-none px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+          <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="text" name="PAN" placeholder='Enter PAN' className='outline-none px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+          <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="text" name="GSTNo" placeholder='Enter GSTNo' className='outline-none px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+          <div className='h-10 flex justify-center items-center  border-2 rounded-md w-full px-4 '>
+              <input type="text" name="clientBankName" placeholder='Enter clientBankName' className='outline-none px-9 h-7 w-full' onChange={handleChange}/>
+              
+          </div>
+    
+          
+    
+            <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm bg-black hover:bg-[#555] active:bg-lime-500 active:scale-[0.9]' onChange={handleChange}>
+            <button className='h-10 text-white font-bold text-md tracking-widest w-full'>click</button>
+          </div>
+          </form>
+        </div>
   )
 }
 
--- bulidapp/src/components/user/main/navbar/Navbar.jsx
+++ bulidapp/src/components/user/main/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         setshow(!show)
     }
   return (
-    <div className={`w-full h-[80px] bg-black flex justify-around items-center text-xl text-amber-50 gap-4 max-sm:justify-start max-sm:px-4 ${show?'h-[200px] flex flex-col justify-around gap-2.5 items-start':""}sm:flex-row sm:h-[80px] sm:justify-around sm:items-center`}>
+    <div className={`w-full h-[80px] bg-black flex justify-around items-center text-xl text-amber-50 gap-4 max-sm:justify-start max-sm:px-8 ${show?'h-[200px] flex flex-col justify-around gap-2.5 items-start py-2.5':""}sm:flex-row sm:h-[80px] sm:justify-around sm:items-center`}>
 
       <div className='hidden max-sm:flex'>
 
@@ -22,15 +22,16 @@ const Navbar = () => {
         </div>
     
     <div className={`max-sm:hidden${show?'block':""}`} >
-        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/home" className={({isActive})=>isActive?"text-shadow-sm bg-white rounded-sm ease-[2s]":""} end>Home</NavLink>
     </div>
     <div className={`max-sm:hidden${show?'block':""}`}>
-        <NavLink to="addBills">Add Bills</NavLink>
+        <NavLink to="addBills" className={({isActive})=>isActive?"bg-amber-50 text-black rounded-sm":""}>Add Bills</NavLink>
     </div>
-    <div className={`max-sm:hidden${show?'block':""}`}> <NavLink to="filterBills"> Filter Bills</NavLink>
+    <div className={`max-sm:hidden${show?'block':""}`}> 
+        <NavLink to="filterBills" className={({isActive})=>isActive?"bg-amber-50 text-black rounded-sm":""}> Filter Bills</NavLink>
     </div>
     <div className={`max-sm:hidden${show?'block':""}`}>
-         <NavLink to="about">About</NavLink>
+         <NavLink to="about" className={({isActive})=>isActive?"bg-amber-50 text-black rounded-sm":""}>About</NavLink>
     </div>
     </div>
   )
